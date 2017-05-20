d868714354bb604ceb5ebd0185a8d3fa3ca0b6aa
Tong Lin <tonglin.t91@gmail.com>
2017-05-19 22:23:48 -0400

    add nib table view cell

--- MyWeather/MyWeather/Controllers/MainViewController.swift
+++ MyWeather/MyWeather/Controllers/MainViewController.swift
@@ -17,8 +17,9 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        setupTableView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,7 +27,34 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func setupTableView(){
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "HistoryWeatherTableViewCell", bundle: nil), forCellReuseIdentifier: HistoryWeatherTableViewCell.cellIdentifier)
+    }
    
 
 }
+
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: HistoryWeatherTableViewCell.cellIdentifier, for: indexPath) as! HistoryWeatherTableViewCell
+        
+        return cell
+    }
+    
+}
+
+
+
+
+
+
---
+++ MyWeather/MyWeather/Views/HistoryWeatherTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  HistoryWeatherTableViewCell.swift
+//  MyWeather
+//
+//  Created by Tong Lin on 5/19/17.
+//  Copyright © 2017 TongLin91. All rights reserved.
+//
+
+import UIKit
+
+class HistoryWeatherTableViewCell: UITableViewCell {
+
+    static let cellIdentifier = "HistoryLocationCellIdentifier"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
